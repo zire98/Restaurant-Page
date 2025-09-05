b8f1666a63089765f81b2a398e5018091ac16736
zire98 <164895719+zire98@users.noreply.github.com>
2025-09-05 12:52:45 +0200

    feat(): import about page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import createHome from "./pages/Home.js";
-import createMenu from "./pages/Menu.js"
+import createMenu from "./pages/Menu.js";
+import createAbout from "./pages/About.js";
 import './styles.css';
-createMenu();
+createAbout();
