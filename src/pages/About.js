c3fc7b1c3cd47191a06154c54f41877100994c03
zire98 <164895719+zire98@users.noreply.github.com>
2025-09-07 14:58:08 +0200

    feat: add social networks links with img

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,9 @@
-import { createEl } from "../utils/dom.js";
+import { createEl, createImgLink } from "../utils/dom.js";
 import aboutData from "../assets/data/about.json";
 import img from "../assets/images/about.jpg";
-import location from "../assets/images/adress.jpg"
+import location from "../assets/images/adress.jpg";
+import insta from "../assets/images/instagram.jpg";
+import twitter from "../assets/images/x.jpg"
 
 export default function createAbout() {
     const content = document.querySelector("#content");
@@ -17,10 +19,13 @@ export default function createAbout() {
     const subtitle = createEl("h2", { className: "about-subtitle", text: aboutData.subtitle });
     const text = createEl("p", { className: "about-text", text: aboutData.text });
     const adress = createEl("p", { className: "about-text", text: aboutData.adress });
-    const adressImg = createEl("img", { className: "adress-img" });
-    adressImg.src = location;
+    const adressImg = createImgLink(location, "https://www.google.com/maps?hl=es&gl=es&um=1&ie=UTF-8&fb=1&sa=X&ftid=0xd5aabd6d28cc0b7:0x48ab033b37ec5dd3", "adress-link", "adress-img");
+    const socialNetworks = createEl("div", { className: "social-networks", text: "Social Networks" });
+    const instagram = createImgLink(insta, "https://www.instagram.com/", "insta-link", "social-img");
+    const x = createImgLink(twitter, "https://x.com/?lang=es", "x-link", "social-img");
+    socialNetworks.append(instagram, x);
 
-    rightAbout.append(title, subtitle, text, adress, adressImg);
+    rightAbout.append(title, subtitle, text, adress, adressImg, socialNetworks);
     aboutContainer.append(leftAbout, rightAbout);
     content.appendChild(aboutContainer);
 }
